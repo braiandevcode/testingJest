0c6ed7a6177b0997fd64b2fde2abfb40d8fb0daa
Braian Palacios <braianpalacios699@gmail.com>
2025-09-05 07:04:02 -0300

    testing jest

--- src/notebooks/notebooks.service.spec.ts
+++ src/notebooks/notebooks.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotebooksService } from './notebooks.service';
+import { Notebook } from './entities/notebook.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('NotebooksService', () => {
   let service: NotebooksService;
 
+  const mockRepo = {
+    find: jest.fn().mockResolvedValue([]),
+    save: jest.fn().mockResolvedValue({ id: 1, title: 'Test', content: 'Contenido' }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NotebooksService],
+      providers: [
+        NotebooksService,
+        {
+          provide: getRepositoryToken(Notebook),
+          useValue: mockRepo,
+        },
+      ],
     }).compile();
 
     service = module.get<NotebooksService>(NotebooksService);
@@ -16,3 +29,4 @@ describe('NotebooksService', () => {
     expect(service).toBeDefined();
   });
 });
+
